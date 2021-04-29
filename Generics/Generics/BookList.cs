ae87e7f6b638d0680e7dc3b1e5f32328534d0fc8
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2021-04-29 14:39:29 +0530

    Add files via upload

---
+++ Generics/Generics/BookList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Generics
+{
+    public class BookList
+    {
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    public class GenericDictionary<TKey, TValue> //GenericDictionary
+    {
+        public void Add(TKey key, TValue value)
+        {
+
+        }
+    }
+
+    public class GenericList<T> //GenericList
+    {
+        public void Add(T value) // we dont know what T is yet
+        {
+
+        }
+
+        public T this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Generics/Generics/DiscountCalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Generics
+{
+    // t to a class
+    public class DiscountCalculator<TProduct> where TProduct : Product
+    {
+        public float CalculateDiscount(TProduct product)
+        {
+            return product.Price;
+        }
+    }
+}
